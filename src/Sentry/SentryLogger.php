e018784fd99664da47a8bf6d17c8463a46e84a96
Josef Benjač <josefbenjac@gmail.com>
2018-11-12 17:47:43 +0100

    SentryLogger: added priority and critical string messages

--- src/Sentry/SentryLogger.php
+++ src/Sentry/SentryLogger.php
@@ -9,33 +9,67 @@ use Throwable;
 class SentryLogger implements ILogger
 {
 
-	/** @var mixed[] */
-	private $config;
+	public const LEVEL_PRIORITY_MAP = [
+		self::DEBUG => Raven_Client::DEBUG,
+		self::INFO => Raven_Client::INFO,
+		self::WARNING => Raven_Client::WARNING,
+		self::ERROR => Raven_Client::ERROR,
+		self::EXCEPTION => Raven_Client::FATAL,
+		self::CRITICAL => Raven_Client::FATAL,
+	];
+
+	/** @var string */
+	private $url;
 
 	/**
-	 * @param mixed[] $config
+	 * @param string $url
 	 */
-	public function __construct(array $config)
+	public function __construct(string $url)
 	{
-		$this->config = $config;
+		$this->url = $url;
 	}
 
 	/**
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingReturnTypeHint
 	 * @param mixed $message
 	 */
 	public function log($message, string $priority = ILogger::INFO): void
 	{
-		if (!in_array($priority, [ILogger::ERROR, ILogger::EXCEPTION, ILogger::CRITICAL], true)) return;
-		if (!($message instanceof Throwable)) return;
+		if (!in_array($priority, [ILogger::ERROR, ILogger::EXCEPTION, ILogger::CRITICAL], TRUE)) return;
 
-		// Send to Sentry
-		$this->makeRequest($message);
+		$level = $this->getLevel($priority);
+
+		if ($level === NULL) return;
+
+		$data = [
+			'level' => $level,
+		];
+
+		$this->makeRequest($message, $data);
 	}
 
-	protected function makeRequest(Throwable $message): void
+	/**
+	 * @param mixed $message
+	 * @param mixed[] $data
+	 */
+	protected function makeRequest($message, array $data): void
 	{
-		$client = new Raven_Client($this->config['url']);
-		$client->captureException($message);
+		$client = new Raven_Client($this->url);
+		if ($message instanceof Throwable) {
+			$client->captureException($message, $data);
+		} else {
+			$client->captureMessage($message, [], $data);
+		}
+	}
+
+	/**
+	 * @param string $priority
+	 * @return string|null
+	 */
+	protected function getLevel(string $priority): ?string
+	{
+		return self::LEVEL_PRIORITY_MAP[$priority] ?? NULL;
 	}
 
 }
